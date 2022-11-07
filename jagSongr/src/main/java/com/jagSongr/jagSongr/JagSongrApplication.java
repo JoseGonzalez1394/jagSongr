66a3fbd3d48184a8030365ba0e3c9d35c7404261
Jose A Gonzalez <JGman13@gmail.com>
2022-11-07 12:59:51 -1000

    initial commit from Spring init

---
+++ jagSongr/src/main/java/com/jagSongr/jagSongr/JagSongrApplication.java
@@ -0,0 +1,13 @@
+package com.jagSongr.jagSongr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagSongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagSongrApplication.class, args);
+	}
+
+}
---
+++ jagSongr/src/test/java/com/jagSongr/jagSongr/JagSongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagSongr.jagSongr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagSongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
